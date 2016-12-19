8701fb4ef1c626c19733ef651bd2448a388dc725
jgretz <jgretz@truefitsolutions.com>
2016-12-18 20:24:17 -0500

    add check for illegal verbs

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,18 @@ import express from 'express';
 
 const BEFORE = 'before';
 const AFTER = 'after';
+const VERBS = ['get','put','post','delete'];
 
 export default (app, config) => {
   var router = express.Router();
 
   _.forEach(config.routes, (routeDefinition) => {
     const { verb, route, implementation } = routeDefinition;
+
+    // only accept certain verbs
+    if (!VERBS.includes(verb.toLowerCase())) {
+      return;
+    }
 
     router[verb](route, (req, res) => {
       if (implementation[BEFORE]) {
