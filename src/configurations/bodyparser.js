ed2b0f99615f325fecc2fd011b18d467a9a2016e
jgretz <jgretz@truefitsolutions.com>
2017-03-25 13:45:38 -0400

    add default param to config

--- src/configurations/bodyparser.js
+++ src/configurations/bodyparser.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import _bodyParser from 'body-parser';
 
-export const bodyParser = config => app => {
+export const bodyParser = (config = {}) => app => {
   _.extend(config, {type: 'application/json'});
   app.use(_bodyParser.json(config));
 };
